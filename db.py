4ffd97ef06aa05ceab939a749fb383c11a74f52a
annesh0 <67394141+annesh0@users.noreply.github.com>
2022-05-01 18:29:50 -0400

    Update db.py

--- db.py
+++ db.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 # pprint library is used to make the output look more pretty
 from pprint import pprint
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient("mongodb+srv://CUSolarBoat:Cu2uiQrlwlfZG2gJ@cluster0.lkpux.mongodb.net/Test",tls=True, tlsAllowInvalidCertificates=True)
-db=client.Test
+client = MongoClient("mongodb+srv://CUSolarBoat:Cu2uiQrlwlfZG2gJ@cluster0.lkpux.mongodb.net/test",tls=True, tlsAllowInvalidCertificates=True)
+db=client.DB
 # Issue the serverStatus command and print the results
 serverStatusResult=db.command("serverStatus")
 #pprint(serverStatusResult)
@@ -12,6 +12,7 @@ test = {
     "V1" : "1",
     "V2" : "1",
     "V3" : "1",
+    "V4" : "1",
     "Current" : "1"
 }
 result = db.id.insert_one(test)
