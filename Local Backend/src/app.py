0ddb67be8ba032894395ecea2ca0d670e3e6aa11
annesh0 <67394141+annesh0@users.noreply.github.com>
2022-11-01 19:12:42 -0400

    removing unnecessary print statements

--- Local Backend/src/app.py
+++ Local Backend/src/app.py
@@ -1,8 +1,3 @@
-from serial import Serial
-import json
-import time
-from datetime import datetime
-
 from db import db
 from db import Logs
 from db import Readings
@@ -10,8 +5,6 @@ from sqlalchemy.sql.expression import func
 from flask import Flask
 import json
 from flask import request
-# pprint library is used to make the output look more pretty
-from pprint import pprint
 
 app = Flask(__name__)
 db_filename = "Sysco.db"
@@ -39,8 +32,6 @@ def hello():
     return success_response(
         "Hello World"
     )
-
-print("I am here")
 
 #get all logs route
 @app.route("/all-logs")
@@ -120,140 +111,10 @@ def delete_reading(log_id, reading_index):
     db.session.commit()
     return success_response(reading.serialize())
 
-# @app.route("/api/users/", methods=["POST"])
-# def create_user():
-#     body = json.loads(request.data)
-#     if not body.get("name") or not body.get("netid"):
-#         return failure_response("not all fields were provided!", 400)
-#     new_user = User(name=body.get("name"), netid=body.get("netid"))
-#     db.session.add(new_user)
-#     db.session.commit()
-#     return success_response(new_user.serialize(), 201)
-#
-# @app.route("/api/users/<int:user_id>/")
-# def get_user(user_id):
-#     user = User.query.filter_by(id=user_id).first()
-#     if user is None:
-#         return failure_response("User not found!")
-#     return success_response(user.serialize())
-#
-# @app.route("/api/courses/<int:course_id>/add/", methods=["POST"])
-# def add_user(course_id):
-#     body = json.loads(request.data)
-#     course = Course.query.filter_by(id=course_id).first()
-#     if course is None:
-#         return failure_response("Course not found!")
-#     if not body.get("user_id"):
-#         return failure_response("user not provided!", 400)
-#     if body.get("type") != "student" and body.get("type") != "instructor":
-#         return failure_response("invalid user type", 400)
-#     user = User.query.filter_by(id=body.get("user_id")).first()
-#     if user is None:
-#         return failure_response("User not found!")
-#     for person in course.students:
-#         if person.id == user.id:
-#             return failure_response("already in the class!", 400)
-#     for person in course.instructors:
-#         if person.id == user.id:
-#             return failure_response("already in the class!", 400)
-#     if body.get("type") == "student":
-#         course.students.append(user)
-#     if body.get("type") == "instructor":
-#         course.instructors.append(user)
-#     db.session.commit()
-#     course = Course.query.filter_by(id=course_id).first()
-#     return success_response(course.serialize())
-#
-# @app.route("/api/courses/<int:course_id>/assignment/", methods=["POST"])
-# def add_assignment(course_id):
-#     body = json.loads(request.data)
-#     course = Course.query.filter_by(id=course_id).first()
-#     if course is None:
-#         return failure_response("Course not found!")
-#     if not body.get("title") or not body.get("due_date"):
-#         return failure_response("not all fields filled", 400)
-#     assignment = Assignment(title=body.get("title"), due_date=body.get("due_date"), course_id=course_id)
-#     course = Course.query.filter_by(id=course_id).first()
-#     course.assignments.append(assignment)
-#     db.session.add(assignment)
-#     db.session.commit()
-#     return success_response(assignment.serialize(), 201)
-#
 @app.route("/reset/")
 def reset():
     db.drop_all()
     db.create_all()
-    return success_response("yes")
-
-@app.route("/collect/")
-def collect():
-    #s = Serial(port='/dev/cu.usbmodem11101', baudrate=9600)
-    print("1.1")
-    s = Serial(port='COM4', baudrate=9600, timeout=.1)
-    print("1.2")
-    s.flushInput()
-    print("1.3")
-    # Serial.close(s)
-    print("1.4")
-    new_log = Logs(time= str(datetime.now()))
-    print("1.5")
-    db.session.add(new_log)
-    print("1.6")
-    db.session.commit()
-    print("1.7")
-    parent_log = Logs.query.filter_by(id=db.session.query(func.max(Logs.id))).first()
-    print("1.8")
-
-    # client = MongoClient("mongodb+srv://CUSolarBoat:Cu2uiQrlwlfZG2gJ@cluster0.lkpux.mongodb.net/test",tls=True, tlsAllowInvalidCertificates=True)
-    # db=client.DB
-    # logDic = {}
-    # logDic["log"] = json.loads(json.dumps(list(db.id.find({}, {'_id': False, "Timestamp":False}))))
-    # logDic["Timestamp"] = str(datetime.now())
-    # db.logs.insert_one(logDic)
-    # db["id"].drop()
-    # timeIndex = 0
-    #         # Issue the serverStatus command and print the results
-    # serverStatusResult=db.command("serverStatus")
-    print("I am here 2")
-    time.sleep(1)
-    while True:
-        try:
-            ser_bytes = str(s.readline())
-            time.sleep(1)
-            ser_bytes = ser_bytes[3:len(ser_bytes) - 6]
-        # print(ser_bytes)
-            voltages = ser_bytes.split(", ")
-            voltageDic = {}
-            voltageDic["Index"] = len(parent_log.Readings)
-            for v in voltages:
-                voltageDic[v[0:2]] = float(v[4:])
-            #print(voltageDic['V1'])
-            #voltageDic['V4'] = 0.0
-            voltageDic['Current'] = 0.0
-            voltageDic["Timestamp"] = str(datetime.now())
-            
-            # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-            
-            #pprint(serverStatusResult)
-            '''
-            test = {
-                "Time" : "16",
-                "V1" : "1",
-                "V2" : "1",
-                "V3" : "1",
-                "V4" : "1",
-                "Current" : "1"
-            }'''
-            new_reading = Readings(time= voltageDic.get("Timestamp"), V1=voltageDic.get("V1"), V2=voltageDic.get("V2"), V3=voltageDic.get("V3"), C=voltageDic.get("Current"), index= voltageDic.get("Index"), log_id=parent_log.id)
-            parent_log.Readings.append(new_reading)
-            db.session.add(new_reading)
-            db.session.commit()
-            #print(voltages)
-        except Exception as e:
-            
-            print(e)
-            #print("Keyboard Interrupt")
-            break
     return success_response("yes")
 
 if __name__ == "__main__":
