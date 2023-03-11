f2a4c2cbe370425b23e65aedc4bb7ff98967efeb
tsangha03 <tsangha03@gmail.com>
2023-03-11 14:43:48 -0500

    Implemented function that creates data.json file

--- Dashboard/httpget.js
+++ Dashboard/httpget.js
@@ -1,27 +1,19 @@
+const axios = require('axios');
+const fs = require('fs');
 /*used to request data from the url as type ...*/
 function httpget(url) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", url, false);
-  xmlHttp.send(null);
-  return xmlHttp.responseText;
+  axios.get(url)
+    .then(response => {
+      fs.writeFile('data.json', JSON.stringify(response.data), function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Data written to file');
+        }
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
-
-function getUrlVars(url) {
-  var hash;
-  var myJson = {};
-  var hashes = url.slice(url.indexOf('?') + 1).split('&');
-  for (var i = 0; i < hashes.length; i++) {
-    hash = hashes[i].split('=');
-    myJson[hash[0]] = hash[1];
-    // If you want to get in native datatypes
-    // myJson[hash[0]] = JSON.parse(hash[1]); 
-  }
-  return myJson;
-}
-
-var params = getUrlVars("http://0.0.0.0:4000/all-logs");
-console.log(params);
-
-
-
-//Console.log(httpget("http://0.0.0.0:4000/all-logs"))
+httpget('http://0.0.0.0:4000/all-logs')
