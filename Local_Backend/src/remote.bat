6e2b2b5835860c77a642f65d2a82a0fdc195473b
annesh0 <94804379+DylanMcCreesh@users.noreply.github.com>
2023-05-06 13:02:04 -0400

    Batch Files

---
+++ Local_Backend/src/remote.bat
@@ -0,0 +1,4 @@
+@echo off
+call "C:\Users\CUSB\Documents\GitHub\2122CUSBSyscoRepo\Local Backend\src\venv\Scripts\activate.bat"
+"C:\Users\CUSB\Documents\GitHub\2122CUSBSyscoRepo\Local_Backend\src\venv\Scripts\python.exe" "C:\Users\CUSB\Documents\GitHub\2122CUSBSyscoRepo\SolarBoat\arduinoVoltage.py"
+@pause
