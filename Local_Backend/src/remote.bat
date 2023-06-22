5d475a8c5643944a30728ebcc6376a75647c9099
esh87 <esh87@cornell.edu>
2023-06-22 17:41:08 -0400

    Removed unnecessary files

--- Local_Backend/src/remote.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Users\CUSB\Documents\GitHub\2122CUSBSyscoRepo\Local Backend\src\venv\Scripts\activate.bat"
-"C:\Users\CUSB\Documents\GitHub\2122CUSBSyscoRepo\Local_Backend\src\venv\Scripts\python.exe" "C:\Users\CUSB\Documents\GitHub\2122CUSBSyscoRepo\SolarBoat\arduinoVoltage.py"
-@pause
--- Local_Backend/src/testcusb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/Documents/GitHub/2122CUSBSyscoRepo/Local_Backend/src/
-source venv/bin/activate
-pip3 install -r requirements.txt
-python3 script.py
