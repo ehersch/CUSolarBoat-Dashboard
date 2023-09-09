54f92126d3d2b4b6df21930f04d7031a2c5c9ed5
Ethan Hersch <herschethan@gmail.com>
2023-09-09 14:51:20 -0400

    Test commit

--- Dashboard/test_graph.py
+++ Dashboard/test_graph.py
@@ -4,29 +4,28 @@ from subprocess import run
 import time
 
 while True:
-    with open('./data.json') as f:
+    with open("./data.json") as f:
         data = json.load(f)
 
-    readings = data['logs'][-1]['Readings']
-    labels = [reading['Timestamp'] for reading in readings]
-    v1Data = [reading['V1'] for reading in readings]
-    v2Data = [reading['V2'] for reading in readings]
-    v3Data = [reading['V3'] for reading in readings]
+    readings = data["logs"][-1]["Readings"]
+    labels = [reading["Timestamp"] for reading in readings]
+    v1Data = [reading["V1"] for reading in readings]
+    v2Data = [reading["V2"] for reading in readings]
+    v3Data = [reading["V3"] for reading in readings]
 
     x_pos = [0, 1, 2]
     voltage_values = [v1Data[-1], v2Data[-1], v3Data[-1]]
     plt.bar(x_pos, voltage_values)
-    plt.xticks(x_pos, ['V1', 'V2', 'V3'])
+    plt.xticks(x_pos, ["V1", "V2", "V3"])
     plt.yticks(range(0, 38, 2))
-    plt.title('Voltage Readings')
-    plt.xlabel('Voltage Reading')
-    plt.ylabel('Voltage')
+    plt.title("Voltage Readings")
+    plt.xlabel("Voltage Reading")
+    plt.ylabel("Voltage")
     for i, v in enumerate(voltage_values):
-        plt.text(i, v+1, str(v), ha='center', fontweight='bold')
+        plt.text(i, v + 1, str(v), ha="center", fontweight="bold")
 
     plt.draw()
-    plt.pause(5)
+    plt.pause(1)
     plt.clf()
 
     run(["node", "httpget.js"])
-
